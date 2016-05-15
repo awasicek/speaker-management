5ce898f42bcb66264a9967ac836961e5217bbcf8
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-05-15 15:10:16 -0700

    basic server.js setup and folder creation

---
+++ server.js
@@ -0,0 +1,35 @@
+var
+  express = require('express'),
+  app = express(),
+  logger = require('morgan'),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser'),
+  // apiRoutes = require('./routes/api.js'),
+  path = require('path')
+
+mongoose.connect('mongodb://localhost/speaker-management', function(err){
+  if(err) return console.log("Errror connecting.")
+  console.log("Connected to MongoDB (speaker-management)")
+})
+
+//middleware begin
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+//middleware end
+
+
+//routes
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+// app.use('/api', apiRoutes)
+
+//server listening
+
+app.listen(3000, function(){
+  console.log("Server running on port 3000.")
+})
