7259460343d7fa86f418c04ec34e606b06bd50f1
Andrew Wasicek <andrew.wasicek@gmail.com>
2016-05-15 16:42:17 -0700

    started setting up angular

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+(function(){
+  angular.module('speakerManagement', ['ui.router'])
+    .config(function($stateProvider, $urlRouterProvider){
+
+      //handle any attempts to routes other than what is listed below
+      $urlRouterProvider.otherwise('/')
+
+      //my established routes
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: '/partials/home.html'
+        })
+        .state('users', {
+          url: '/users',
+          templateUrl: '/partials/users.html',
+          controller: 'MainController as main'
+        })
+        .state('newUser', {
+          url: '/users/new',
+          templateUrl: '/partials/new.html',
+          controller: "MainController as main"
+        })
+        .state('detail', {
+          url: '/users/:id',
+          templateUrl: '/partials/detail.html',
+          controller: 'DetailController as detail'
+        })
+    })
+})()
